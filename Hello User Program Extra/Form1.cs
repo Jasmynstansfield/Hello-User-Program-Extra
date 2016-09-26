b7b881a3627072aaf4bbd91f113fe889c7610738
Jazz <jasmynpaige14@gmail.com>
2016-09-26 10:25:27 -0500

    Assignment 1b
    Almost Complete

---
+++ Hello User Program Extra/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_User_Program_Extra
+{
+    public partial class frmGreetings : Form
+    {
+        public frmGreetings()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSnareDrum_Click(object sender, EventArgs e)
+        {
+            lblJoke.Text = "*Ba-dum ching*";
+        }
+
+        private void btnAmnesia_Click(object sender, EventArgs e)
+        {
+            lblJoke.Text = "I forgot how it goes.";
+        }
+    }
+}
